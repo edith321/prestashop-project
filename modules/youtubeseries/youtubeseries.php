7c09d371d0a7d5ee83c5165d3257b47e7872b4c3
edith321 <edita.paulikaite@inbox.lt>
2017-11-21 14:33:29 +0200

    Erase

--- modules/youtubeseries/youtubeseries.php
+++ modules/youtubeseries/youtubeseries.php
@@ -9,21 +9,33 @@ class youtubeseries extends Module { // reikia sukurti klase su tokiu pat pav ka
     public function __construct()
 	{
 		$this->name = 'youtubeseries'; // name of the plugin
-		$this->tab = 'front_office_features';
-		$this->version = '2.1.2';
-		$this->bootstrap = true;
-		$this->secure_key = Tools::encrypt($this->name);
+		$this->tab = 'front_office_features'; // The title for the section that shall contain this module in PrestaShop's back office modules list
+		$this->version = '1.0';
+		$this->bootstrap = true; // whether to include bootstrap
 		$this->default_language = Language::getLanguage(Configuration::get('PS_LANG_DEFAULT'));
-		$this->languages = Language::getLanguages();
-		$this->author = 'PrestaShop';
+		/*$this->languages = Language::getLanguages();*/ // set the language of our model
+		$this->author = 'Edita';
+        
 		parent::__construct();
-		$this->displayName = $this->l('Theme configurator');
-		$this->description = $this->l('Configure the main elements of your theme.');
-		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
-		$this->module_path = _PS_MODULE_DIR_.$this->name.'/';
+        
+		$this->displayName = $this->l('Youtube Series Module'); // the name that will be displayed in the module's list in admin panel
+		$this->description = $this->l('A list of videos from youtube');
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99'); // which version of prestashop is compatible with this plugin
+/*		$this->module_path = _PS_MODULE_DIR_.$this->name.'/';
 		$this->uploads_path = _PS_MODULE_DIR_.$this->name.'/img/';
 		$this->admin_tpl_path = _PS_MODULE_DIR_.$this->name.'/views/templates/admin/';
-		$this->hooks_tpl_path = _PS_MODULE_DIR_.$this->name.'/views/templates/hooks/';
+		$this->hooks_tpl_path = _PS_MODULE_DIR_.$this->name.'/views/templates/hooks/';*/ 
 	}
     
+    public function install() // installation function
+    {
+       if (!parent::install() || 
+			!$this->registerHook('displayHeader') || //display header and footer
+			!$this->registerHook('displayFooter') 
+		)
+           return false; // if any of these conditions fail return false and don't install the module
+           
+        return false;
+    }
 }
+    
